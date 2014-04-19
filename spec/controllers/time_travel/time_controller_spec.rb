bda6289b9a02817448c946afc2d1989b87f3bbd2
Jamie Monserrate <jamiemonserrate@gmail.com>
2014-04-19 22:29:30 +0530

    jMe - Fixing test. The time travel method also sets the time by calling the filter

--- spec/controllers/time_travel/time_controller_spec.rb
+++ spec/controllers/time_travel/time_controller_spec.rb
@@ -19,6 +19,7 @@ describe TimeTravel::TimeController do
       file = double('file')
       expect(file).to receive(:write).with("1,1,2014")
       expect(File).to receive(:open).with("#{Rails.root}/tmp/time_travel.txt", "w").and_yield(file)
+      allow(File).to receive(:open).with("#{Rails.root}/tmp/time_travel.txt").once.and_return(double(read: "1,1,2014")) #for the filter which will set the time
 
       get :travel, use_route: 'time_travel', day: 1, month: 1, year: 2014
 
